254c21b97843576acfeaccc3f3c09a2e1cdc57e1
ShrivatsaSK <kulkarnishrivatsa05@gmail.com>
2026-05-05 16:16:13 +0530

    Improved the code 1 for better usability

--- 1-card_game_winer.cpp
+++ 1-card_game_winer.cpp
@@ -1,8 +1,9 @@
+// Online C++ compiler to run C++ program online
 #include <iostream>
 #include <stdio.h>
+#include <cmath>
 using namespace std;
 
-//Creating a dictionary
 int cardvalue(string card){
     if(card=="A") return 1;
     if(card=="K") return 13;
@@ -10,19 +11,28 @@ int cardvalue(string card){
     if(card=="J") return 11;
     return stoi(card);
 }
-
 int main() {
     int TC=0, MK=0, result=0;
     string card;
+    cout << "Enter player 1 cards played (spae seperated):";
     for(int i=0; i<52; i++){
         cin >> card;
         TC += cardvalue(card);
     }
+    cout << "Enter player 2 cards played (spae seperated):";
     for (int j=0; j<52; j++){
         cin>>card;
         MK += cardvalue(card);
     }
     result = TC - MK;
-    cout << result <<endl;
+    if (result > 0) {
+        cout << "Player 1 won the game by " << result << " points " <<endl;
+    }
+    else if (result < 0) {
+        cout <<"Player 2 won the game by " << abs(result) << " points " << endl; // abs() is used to print negative number as positive
+    }
+    else if (result == 0) {
+        cout <<" The game is a Tie" << endl;
+    }
     return 0;
 }
