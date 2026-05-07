5c354c09615c2a094e4aa23b0df0898a1e41da6f
ShrivatsaSK <kulkarnishrivatsa05@gmail.com>
2026-05-08 00:32:50 +0530

    ..

--- CPP/4-reverse of a number.cpp
+++ CPP/4-reverse of a number.cpp
@@ -9,13 +9,7 @@ int main(){
         rem = num%10;
         reverse = reverse * 10 + rem;
         num /=10;
-    }; // reverse logic
-
-    if (num == reverse){
-        cout << "The provided number : " << reverse << " is a palindrome number !";
-    }
-    else if ( num != reverse){
-        cout << "The revered number is : " << reverse ;
-    }
+    };
+    cout << "The revered number is : " << reverse ;
     return 0;
 }
